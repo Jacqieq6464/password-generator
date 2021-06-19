14648851d785388c000afc2567b71b9063e95057
jacqie williamson <jacqieq@me.com>
2021-06-20 00:08:15 +0100

    more work on js script

---
+++ Develop/script2.js
@@ -0,0 +1,37 @@
+//Pseudo code: for JS file
+
+    //ask how long should the password be
+    
+    //MDN - propmt; alert; confirm
+
+
+    //give user choice:-
+
+    //do you want UCase
+    var upperCase = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z']
+
+
+    //do you want LCase
+    var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',  'u',  'v',  'w', 'x', 'y', 'z'];
+
+
+    //do you want numbers
+    var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    //do you want special characters
+    var specialCharacters = [ '@', '%', '+', '\\',  '/',  '!', '#', '$',  '^', '?',  ':', ',',  ')', '(', '}', '{',  ']',  '[',  '~',  '-',  '_',  '.']
+
+
+    //my alerts give popup messages to user
+
+
+
+    //collate and put into 1 group - 
+
+
+
+    //randomly select characters
+
+
+
+//link to html
