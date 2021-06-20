9c31e3689b0c79cc4ca7ab3d8de2ad4f0eabc780
jacqie williamson <jacqieq@me.com>
2021-06-20 01:46:28 +0100

    update js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,28 @@
 // Assignment Code
-//do you want UCase - globally scoped
+//do you want to use Upper Case characters - var = globally scoped
+
 var upperCase = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z']
 
 
-//do you want LCase
+//Array of lower case characters - do you want to use Lower Case
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',  'u',  'v',  'w', 'x', 'y', 'z'];
 
 
-//do you want numbers
+//do you want to use numbers
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-//do you want special characters
+//do you want to use special characters
 var specialCharacters = [ '@', '%', '+', '\\',  '/',  '!', '#', '$',  '^', '?',  ':', ',',  ')', '(', '}', '{',  ']',  '[',  '~',  '-',  '_',  '.']
 
 
-var generateBtn = document.querySelector("#generate");//html line 28
+var generateBtn = document.querySelector("#generate");//id selector = html line 28
 
 function generatePassword(){
   var passwordLength = prompt ("how many characters would you like in your password?")
   console.log(passwordLength)
 //if password <8 alert, if password >128 alert
 
-  //for loop allowing the selection of the characters from the string to be used for the password
+  //for loop allowing the selection of the characters from the array of characters.
   for(var i, i = 0; i < characters.length; i++){
     generatePassword += characters.charAt(math.floor(math.random() * characters.length))
 }
@@ -33,7 +34,7 @@ function writePassword() {
   //var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@€$%^&*()+-:<>?/|\{}";//why are 2 chars blue?
 
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");//html line 22 
+  var passwordText = document.querySelector("#password");//id selector = html line 22 
 
   passwordText.value = password;
 
