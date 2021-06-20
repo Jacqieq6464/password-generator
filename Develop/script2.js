560b5c2a2912982d1ea9acb585323d84a1b58397
jacqie williamson <jacqieq@me.com>
2021-06-20 01:46:04 +0100

    Update js pseudo code

--- Develop/script2.js
+++ Develop/script2.js
@@ -8,18 +8,18 @@
     //give user choice:-
 
     //do you want UCase
-    var upperCase = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z']
+    //var upperCase = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z']
 
 
     //do you want LCase
-    var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',  'u',  'v',  'w', 'x', 'y', 'z'];
+    //var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',  'u',  'v',  'w', 'x', 'y', 'z'];
 
 
     //do you want numbers
-    var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    //var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     //do you want special characters
-    var specialCharacters = [ '@', '%', '+', '\\',  '/',  '!', '#', '$',  '^', '?',  ':', ',',  ')', '(', '}', '{',  ']',  '[',  '~',  '-',  '_',  '.']
+    //var specialCharacters = [ '@', '%', '+', '\\',  '/',  '!', '#', '$',  '^', '?',  ':', ',',  ')', '(', '}', '{',  ']',  '[',  '~',  '-',  '_',  '.']
 
 
     //my alerts give popup messages to user
